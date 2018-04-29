70de653f18630281be2648b1c6990997a59a582d
Helder Burato Berto <helder.burato@gmail.com>
2018-04-29 18:41:05 -0300

    initial commit project

--- README.md
+++ README.md
@@ -1,2 +1,35 @@
-# respper
-📦 A simple response wrapper, best used in NodeJS
+# 📦 Respper
+
+A simple response wrapper, best used in NodeJS.
+
+## Requirements
+
+* [NodeJS](https://nodejs.org/en/).
+
+## Installation
+
+You can get it on npm.
+```
+npm install respper --save
+```
+Or if you`re not into package management, just [download a ZIP](https://github.com/helderburato/respper/archive/master.zip) file.
+
+## Usage
+
+```
+router.get('/', (req, res) => {
+  Respper.wrapper(res, Model.findAll(req.query));
+});
+```
+
+## Contributing
+
+Please read [CONTRIBUTING.md](./CONTRIBUTING.md) for details on our code of conduct, and the process for submitting pull requests to us.
+
+## Versioning
+
+We use [SemVer](https://semver.org/) for versioning. For the versions available, see the [tags on this repository](https://github.com/helderburato/respper/tags).
+
+## License
+
+[MIT License](./LICENSE) © Helder Burato
---
+++ src/index.js
@@ -0,0 +1,42 @@
+class Respper {
+
+  static wrapper(response, promise) {
+    return promise
+      .then((result) => {
+        if (!result) {
+          Respper.handleResponse(response, {
+            status: 404,
+            send: { message: 'Dados não informados.' },
+            data: result
+          });
+        }
+
+        Respper.handleResponse(response, {
+          status: 200,
+          data: result
+        });
+      })
+      .catch(err => {
+        Respper.handleResponse(response, {
+          status: 400,
+          message: err.message
+        })
+      });
+  }
+
+  static handleResponse(response, options) {
+    const { status, data } = options;
+    const message = Respper.getMessage(options.message);
+
+    if (!data) return response.status(status).send({ message: message });
+    if (data == '1') return response.sendStatus(status);
+    return response.status(status).send(data);
+  }
+
+  static getMessage(message) {
+    return message || 'Dados não informados.';
+  }
+
+}
+
+module.exports = Respper;
